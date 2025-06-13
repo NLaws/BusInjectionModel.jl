a69dabf67aa9ac19af24f7e824066dcf25426360
Nick Laws <laws.nick@gmail.com>
2025-06-12 21:04:48 -0600

    add var_info and constraint_info to the Multiphase Unrelaxed model

--- src/multi_phase_model.jl
+++ src/multi_phase_model.jl
@@ -43,12 +43,28 @@ function build_bim_rectangular!(m::JuMP.AbstractModel, net::Network{MultiPhase},
         end
     end
 
-    # s_j = sum_{k: j~k} (Z[j,k]^{-1})^* ( |v_j|^2 - v_j v_k^*)
+    # document the variables
+    net.var_info[:v] = CommonOPF.VariableInfo(
+        :v,
+        "complex voltage vector",
+        CommonOPF.VoltUnit,
+        (CommonOPF.BusDimension, CommonOPF.TimeDimension, CommonOPF.PhaseDimension)
+    )
+
+    net.var_info[:s0] = CommonOPF.VariableInfo(
+        :s0,
+        "complex net bus power injection at the net.substation_bus",
+        CommonOPF.ComplexPowerUnit,
+        (CommonOPF.BusDimension, CommonOPF.TimeDimension, CommonOPF.PhaseDimension)
+    )
+
+    # define power
+    m[:bus_power_injection_constraints] = Dict()
     for j in busses(net)
 
         if j == net.substation_bus
 
-            @constraint(m, [t in 1:T],
+            m[:bus_power_injection_constraints][j] = @constraint(m, [t in 1:T],
                 s0[j][t] .== sum(
                     diag(
                         v[j][t] * cj( v[j][t]  - phi_ij(j, net, v[k][t]) ) * cj(yij_per_unit(j, k, net))
@@ -64,7 +80,7 @@ function build_bim_rectangular!(m::JuMP.AbstractModel, net::Network{MultiPhase},
 
         phases = phases_connected_to_bus(net, j)
         # have to down-select to phases to avoid bad constraints like 0 = (1.7 - 0.8im)
-        @constraint(m, [t in 1:T],
+        m[:bus_power_injection_constraints][j] = @constraint(m, [t in 1:T],
             sj[t, phases] .== sum(
                 diag(
                     v[j][t] * cj( v[j][t]  - phi_ij(j, net, v[k][t]) ) * cj(yij_per_unit(j, k, net))
@@ -74,4 +90,14 @@ function build_bim_rectangular!(m::JuMP.AbstractModel, net::Network{MultiPhase},
         )    
     end
 
+    # document the constraints
+    c = m[:bus_power_injection_constraints][net.substation_bus][1][1]  # time step 1, phase 1
+    net.constraint_info[:bus_power_injection_constraints] = CommonOPF.ConstraintInfo(
+        :bus_power_injection_constraints,
+        "net power injection definition at each bus",
+        typeof(MOI.get(m, MOI.ConstraintSet(), c)),
+        (CommonOPF.BusDimension, CommonOPF.TimeDimension, CommonOPF.PhaseDimension),
+    )
+
+    nothing
 end
